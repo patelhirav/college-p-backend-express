d335755edc886e75ee2953e4ad325f6595e67a8d
patelhirav <patelhirav2212@gmail.com>
2025-03-22 17:02:15 +0530

    api url to get

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/otp', otpRoutes);
 
+app.get("/", (req, res) => {
+  res.send(`
+      <h1>Welcome to College API 🎓</h1>
+      <p>This is a backend service for OTP authentication.</p>
+      <p>Check <a href="/api-docs">API Documentation</a> for available endpoints.</p>
+      <p>Login - https://college-p-backend-express.onrender.com/login </p>
+      <p>Login - https://college-p-backend-express.onrender.com/signup </p>
+  `);
+});
+
+
 app.listen(4040, () => {
   console.log('Server running at http://localhost:4040');
 })
